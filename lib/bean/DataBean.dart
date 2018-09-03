685010912c8e7cbeb847f393fe741289a3803402
win7 <737141437@qq.com>
2018-09-03 18:16:27 +0800

    1、提交代码！ 回家搞

--- lib/bean/DataBean.dart
+++ lib/bean/DataBean.dart
@@ -8,16 +8,25 @@ class DataBean{
    //转化data
   static List<DataBean> decodeData(String data){
     List<DataBean> datas=new List<DataBean>();
-
     var newData=json.decode(data);
     var results= newData['message'];
     for(int i=0;i<results.length;i++){
-      datas.add(results[i]);
+      datas.add(forMap(results[i]));
     }
    // print(datas[0].key);
     print(datas[0].message);
     return datas;
 
+  }
+  //{key: smooth, value: 1131, means: [{part: adj., means: [光滑的, 流畅的, 柔软的, 温和的，安详的]}, {part: vt., means: [使平滑, 排除，消除, 安抚，平息, 使优雅]}, {part: vi., means: [变平和，变缓和]}, {part: n., means: [平地，平面]}]}
+  static DataBean forMap(result) {
+    print(result);
+    //这是Json的数据
+    var key= result["key"];
+    var value=result["value"];
+    List means= result["means"];
+    //var mean = means["means"];
+    return null;
   }
 
 }
--- lib/home/TabOne.dart
+++ lib/home/TabOne.dart
@@ -1,4 +1,3 @@
-
 import 'dart:convert';
 import 'dart:io';
 
@@ -6,44 +5,45 @@ import 'package:flutter/material.dart';
 import 'package:flutter_app/bean/DataBean.dart';
 import 'package:flutter_app/widget/CustomButton.dart';
 
-
-class TabOne extends StatefulWidget{
+class TabOne extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
     return new TabOneState();
   }
 }
-class TabOneState extends State<TabOne>{
 
+class TabOneState extends State<TabOne> {
   @override
   void initState() {
     super.initState();
     //http://dict-mobile.iciba.com/interface/index.php?c=word&m=getsuggest&nums=10&client=6&is_need_mean=1&word=sm
-     //我的 Api的地址
+    //我的 Api的地址
     getApiData();
   }
+
   @override
   Widget build(BuildContext context) {
     //return new Text("nihao");
     // api 的接口在这里哦  https://github.com/jokermonn/-Api/blob/master/KingsoftDic.md
-    return new Container(child: new CustomButton("nihao"),);
-  }
-   // 网络请求
-  void getApiData() async{
-      // 注意导入的包的地方是  import 'dart:io';
-      var httpClient=new HttpClient();
-      var url="http://dict-mobile.iciba.com/interface/index.php?c=word&m=getsuggest&nums=10&client=6&is_need_mean=1&word=sm";
-     var request= await httpClient.getUrl(Uri.parse(url));
-     var response= await request.close();
-     if(response.statusCode==HttpStatus.OK){
-       var jsonData= await response.transform(utf8.decoder).join();
-       setState(() {
-         print(jsonData);
-         DataBean.decodeData(jsonData);
-       });
-     }
-
-
+    return new Container(
+      child: new CustomButton("nihao"),
+    );
   }
 
+  // 网络请求
+  void getApiData() async {
+    // 注意导入的包的地方是  import 'dart:io';
+    var httpClient = new HttpClient();
+    var url =
+        "http://dict-mobile.iciba.com/interface/index.php?c=word&m=getsuggest&nums=10&client=6&is_need_mean=1&word=sm";
+    var request = await httpClient.getUrl(Uri.parse(url));
+    var response = await request.close();
+    if (response.statusCode == HttpStatus.OK) {
+      var jsonData = await response.transform(utf8.decoder).join();
+      setState(() {
+        //print(jsonData);
+        DataBean.decodeData(jsonData);
+      });
+    }
+  }
 }
