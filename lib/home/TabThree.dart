8b20de8fe906cc2409c1403f8524958ea7458d40
Administrator <737141437@qq.com>
2018-09-10 19:05:39 +0800

    1、完成我的页面的功能，明天完成博客

--- lib/home/TabThree.dart
+++ lib/home/TabThree.dart
@@ -28,77 +28,82 @@ class TabThreeState extends State<TabThree>{
                 textcolor: '#ffffff');
           },
           child: new Container(
-            padding: const EdgeInsets.only(top: 8.0, bottom: 8.0),
-            child: new Row(
-              children: <Widget>[
-                new Expanded(
-                    flex: 1,
-                    child: new Column(
-                      children: <Widget>[
-                        new Container(
-                          child: new Text(
-                              question.order,
-                              style: new TextStyle(
-                                  color: question.order.compareTo("03") <= 0 ? Colors.red : Colors.yellow,
-                                  fontSize: 18.0
-                              )
-                          ),
-                          alignment: Alignment.topLeft,
-                        ),
-                        question.rise != null ?
-                        new Row(
-                          children: <Widget>[
-                            new Icon(Icons.arrow_upward, color: Colors.red, size: 10.0,),
-                            new Text(question.rise, style: new TextStyle(color: Colors.red, fontSize: 10.0),)
-                          ],
-                        ) : new Container()
-                      ],
-                    )
-                ),
-                new Expanded(
-                    flex: 6,
-                    child: new Column(
-                      children: <Widget>[
-                        new Container(
-                          child: new Text(
-                            question.title,
-                            style: new TextStyle(fontWeight: FontWeight.bold, fontSize: 16.0, height: 1.1, color:  Colors.brown ),
-                          ),
-                          padding: const EdgeInsets.only(bottom: 10.0,right: 4.0),
-                          alignment: Alignment.topLeft,
-                        ),
-                        question.mark != null ?
-                        new Container(
-                            child: new Text(question.mark, style: new TextStyle(color:Colors.white12)),
-                            alignment: Alignment.topLeft,
-                            padding: const EdgeInsets.only(bottom: 8.0,right: 4.0)
-                        ) : new Container(),
-                        new Container(
-                          child: new Text(question.hotNum, style: new TextStyle(color: Colors.white12)),
-                          alignment: Alignment.topLeft,
+      padding: const EdgeInsets.only(top: 8.0, bottom: 8.0),
+      child: new Row(
+        children: <Widget>[
+          new Expanded(
+              flex: 1,
+              child: new Column(
+                children: <Widget>[
+                  new Container(
+                    child: new Text(
+                        question.order,
+                        style: new TextStyle(
+                            color: question.order.compareTo("03") <= 0 ? Colors.red : Colors.yellow,
+                            fontSize: 18.0
                         )
-                      ],
-                    )
-                ),
-                new Expanded(
-                    flex: 3,
-                    child: new AspectRatio(
-                        aspectRatio: 3.0 / 2.0,
-                        child: new Container(
-                          foregroundDecoration:new BoxDecoration(
-                              image: new DecorationImage(
-                                //todo 我喜欢
-                                image: new NetworkImage(question.imgUrl),
-                                centerSlice: new Rect.fromLTRB(270.0, 180.0, 1360.0, 730.0),
-                              ),
-                              borderRadius: const BorderRadius.all(const Radius.circular(6.0))
-                          ),
-                        )
-                    )
-                )
-              ],
-            ),
-          )
+                    ),
+                    alignment: Alignment.topLeft,
+                  ),
+                  question.rise != null ?
+                  new Row(
+                    children: <Widget>[
+                      new Icon(Icons.arrow_upward, color: Colors.red, size: 10.0,),
+                      new Text(question.rise, style: new TextStyle(color: Colors.red, fontSize: 10.0),)
+                    ],
+                  ) : new Container()
+                ],
+              )
+          ),
+      new Expanded(
+              flex: 6,
+              child: new Column(
+                children: <Widget>[
+                  new Container(
+                    child: new Text(
+                      question.title,
+                      style: new TextStyle(fontWeight: FontWeight.bold, fontSize: 16.0, height: 1.1, color:  Colors.brown ),
+                    ),
+                    padding: const EdgeInsets.only(bottom: 10.0,right: 4.0),
+                    alignment: Alignment.topLeft,
+                  ),
+                  question.mark != null ?
+                  new Container(
+                      child: new Text(question.mark, style: new TextStyle(color:Colors.white12)),
+                      alignment: Alignment.topLeft,
+                      padding: const EdgeInsets.only(bottom: 8.0,right: 4.0)
+                  ) : new Container(),
+                  new Container(
+                    child: new Text(question.hotNum, style: new TextStyle(color: Colors.white12)),
+                    alignment: Alignment.topLeft,
+                  )
+                ],
+              )
+          ),
+          new Expanded(
+              flex: 3,
+              child: new AspectRatio(
+                  aspectRatio: 3.0 / 2.0,
+                  child: new Container(
+                    foregroundDecoration:new BoxDecoration(
+                        image: new DecorationImage(
+                          //todo 我喜欢
+                          image: new NetworkImage(question.imgUrl),
+                          centerSlice: new Rect.fromLTRB(270.0, 180.0, 1360.0, 730.0),
+                        ),
+                        borderRadius: const BorderRadius.all(const Radius.circular(6.0))
+                    ),
+                  )
+              )
+          ),
+
+
+
+
+
+        ],
+      ),
+    )
       ),
     );
   }
--- lib/my/MyPage.dart
+++ lib/my/MyPage.dart
@@ -3,49 +3,71 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
-class MyPage extends  StatefulWidget{
+class MyPage extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
     return new MyPageState();
   }
-
 }
-class MyPageState extends State<MyPage>{
-  List widgets = [];
 
+class MyPageState extends State<MyPage> {
+  List widgets = [];
   @override
   void initState() {
     super.initState();
-
-    loadData();
   }
 
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-        appBar: new AppBar(
-          title: new Text("关于我",style: new TextStyle(color: Colors.purple),),
+      appBar: new AppBar(
+        title: new Text(
+          "关于我",
+          style: new TextStyle(color: Colors.purple),
         ),
-        body: new ListView.builder(
-            itemCount: widgets.length,
-            itemBuilder: (BuildContext context, int position) {
-              return getRow(position);
-            }));
-  }
-
-  Widget getRow(int i) {
-    return new Padding(
-        padding: new EdgeInsets.all(10.0),
-        child: new Text("Row ${widgets[i]["title"]}",style: new TextStyle(color: Colors.orange,fontSize: 18.00),)
+      ),
+      body: findBody(),
     );
   }
 
-  loadData() async {
-    String dataURL = "https://jsonplaceholder.typicode.com/posts";
-    http.Response response = await http.get(dataURL);
-    setState(() {
-      widgets = JSON.decode(response.body);
-    });
+  findBody() {
+    return new ListView(
+      children: <Widget>[
+        new Text(
+          "GitHub:https://github.com/Shimingli",
+          style: new TextStyle(color: Colors.lightBlue, fontSize: 18.0),
+        ),
+        new AspectRatio(
+          child: new Container(
+            foregroundDecoration: new BoxDecoration(
+                image: new DecorationImage(
+                  image: new NetworkImage(
+                      "https://upload-images.jianshu.io/upload_images/5363507-ff86aa4df080c21f.png?imageMogr2/auto-orient/strip%7CimageView2/2/w/1240"),
+                  centerSlice: new Rect.fromLTRB(400.0, 400.0, 1360.0, 1500.0),
+                ),
+                borderRadius:
+                    const BorderRadius.all(const Radius.circular(6.0))),
+          ),
+          aspectRatio: 3.0 / 2.0,
+        ),
+        new Text(
+          "简书:https://www.jianshu.com/",
+          style: new TextStyle(color: Colors.lightBlue, fontSize: 18.0),
+        ),
+        new AspectRatio(
+          child: new Container(
+            foregroundDecoration: new BoxDecoration(
+                image: new DecorationImage(
+                  image: new NetworkImage(
+                      "https://upload-images.jianshu.io/upload_images/5363507-b3550172157c83d2.png?imageMogr2/auto-orient/strip%7CimageView2/2/w/1240"),
+                  centerSlice: new Rect.fromLTRB(400.0, 400.0, 1360.0, 1500.0),
+                ),
+                borderRadius:
+                const BorderRadius.all(const Radius.circular(6.0))),
+          ),
+          aspectRatio: 3.0 / 2.0,
+        ),
+      ],
+    );
   }
-
 }
